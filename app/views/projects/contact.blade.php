304d9b70c5bfbdeca36d459477f19068a15134c7
backpup <rknhimura@yahoo.com>
2014-01-30 18:09:36 -0500

    general pages done

--- app/views/projects/contact.blade.php
+++ app/views/projects/contact.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.default')
 
 @section('content')
+<div class="mainContact">
+<h2>Hello</h2>
+{{ Form::open(array('url'=>'contact', 'id'=>'contactForm')) }}
 
-Buzz me
+	{{ Form::label('name', 'Name:') }}
+	{{ Form::text('name', '', array('tabindex'=>10)) }}
 
+
+	{{ Form::label('email', 'Email:') }}
+	{{ Form::email('email') }}<br />
+
+
+
+	{{ Form::label('message', 'Message:') }}
+	{{ Form::textarea('message', '', array('rows'=>5)) }}
+
+
+	{{ Form::submit('send') }}
+{{ Form::close() }}
+</div>
 @endsection
--- app/views/projects/profile.blade.php
+++ app/views/projects/profile.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.default')
 
 @section('content')
-Hello ME
+<article class="main">
+	<h1>About Me</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, nulla, architecto laborum sequi nostrum repudiandae quis amet ab! Harum, maiores, sed tenetur nihil numquam quae maxime accusamus sunt mollitia eligendi!</p>
+
+</article>
 @endsection
